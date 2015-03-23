1a6071afeba39c6d71e98150ae80413904aae754
Faheem Patel <faheem@faheempatel.com>
2015-03-23 20:03:44 +0000

    Display messages on load

--- social/static/social/ajax.js
+++ social/static/social/ajax.js
@@ -2,8 +2,7 @@ $(function(){
 
 	$('#user').blur(function(){
 		$.ajax({
-			type: 'POST',
-			url: '/social/checkuser/',
+			type: 'POST', url: '/social/checkuser/',
 			data : {
 				'user' : $('#user').val(),
 				'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
@@ -34,11 +33,23 @@ function getMessages(view) {
     dataType: 'json'
   });
 }
+
 function displayMessages(data) {
-  json = JSON.parse(data);
-  json.map(function(message) {
-    console.log(message);
-    /*$('body').append("<p>" + message.fields.text + "</p>");*/
-  });
+  // Display messages in reverse chronological order
+  JSON
+    .parse(data)
+    .reverse()
+    .map(function(message) {
+      var li = "<li>";
+      var date = new Date(message.fields.time);
+      var author = '<a href=/social/messages/?view=' + message.fields.auth + '>' + message.fields.auth + '</a>'; 
+      var recipient = message.fields.recip;
+      var text = '"' + message.fields.text + '"';
+      var type = (message.fields.private) ? 'whispered' : 'wrote';
+
+      // ADD ERASE!
+      li = li + date.toUTCString() + ': ' + author + ' ' + type + ': ' + text + '</p></li>';
+      $('#messages ul').append(li);
+    });
 }
 
