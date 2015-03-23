f39fcd52370c54d037effcd8812ec3fb08f8c8f7
Eric John Juta <ericjohnjuta@gmail.com>
2015-03-23 00:37:36 +0000

    Finished messages display forms for all members

--- social/forms.py
+++ social/forms.py
@@ -2,5 +2,7 @@ from django import forms
 from social.models import Message
 
 class MessageForm(forms.Form):
-    text = forms.CharField(label='lol')
-    private = forms.IntegerField()
+    text = forms.CharField(label='Type here to leave a message',
+                           widget=forms.Textarea(attrs={'cols': 35, 'rows': 5}))
+    CHOICES=[('0','Public'),('1','Private')]
+    private = forms.ChoiceField(label='',initial=0,choices=CHOICES, widget=forms.RadioSelect())
